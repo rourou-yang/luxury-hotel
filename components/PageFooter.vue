03ebaa537e8d1480cdf5157378a40ad643f1603f
innie <innie902@gmail.com>
2024-01-01 22:34:25 +0800

    feat: 建立基本頁面

---
+++ components/PageFooter.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="web-container">
+    footer
+  </footer>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped lang="stylus">
+
+.web-container
+  width 100%
+  max-width 1320px
+  padding 0 12px
+
+footer
+  padding 80px 12px
+  background-color #140F0A
+  color #fff
+</style>
---
+++ components/PageHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <header>
+      header
+    </header>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped lang="stylus">
+header
+  height 30px
+  background-color #eee
+</style>
