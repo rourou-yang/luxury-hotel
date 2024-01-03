1c7f4979010d8f9301d8264bfefc91694dde8a36
innie <innie902@gmail.com>
2024-01-03 22:09:29 +0800

    feat: 環境建置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import vitePluginStylusAlias from 'vite-plugin-stylus-alias'
+import app from './app.config'
 
 export default defineNuxtConfig({
+  app,
   build: {
     transpile: ['vuetify'],
   },
@@ -20,9 +23,18 @@ export default defineNuxtConfig({
         transformAssetUrls,
       },
     },
+    plugins: [
+      vitePluginStylusAlias()  // 解决vite中Stylus无法使用@别名的问题
+    ]
+    // resolve: {
+    //   alias: {
+    //     // 调整别名，避免与 Stylus 中的命名冲突
+    //     '@': '/src',
+    //   },
+    // },
   },
   css: [
     '~/assets/common/index.styl',
   ],
-  devtools: { enabled: true }
+  devtools: { enabled: true },
 })
---
+++ plugins/vite-plugin-stylus-alias.d.ts
@@ -0,0 +1,2 @@
+// 為了讓 TypeScript 認得 'vite-plugin-stylus-alias'
+declare module 'vite-plugin-stylus-alias';
