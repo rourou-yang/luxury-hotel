0a030b89863aa11294f534af272ba06826480a81
innie <innie902@gmail.com>
2024-01-06 14:50:27 +0800

    feat: footer 切版

--- components/PageFooter.vue
+++ components/PageFooter.vue
@@ -1,12 +1,39 @@
 <template>
-  <footer class="web-container">
-    <div class="top">
-      <div class="side">
-        left
+  <footer class="web-container footer">
+    <div class="top divide-layout">
+      <div>
+        <nuxt-link to="/" class="logo">
+          <img src="~/assets/images/logo.svg" alt="享樂酒店">
+        </nuxt-link>
+        <div class="social-media">
+          <a href="" class="link">
+            <img src="~/assets/images/icon-line.png" alt="">
+          </a>
+          <a href="" class="link">
+            <img src="~/assets/images/icon-ig.png" alt="">
+          </a>
+        </div>
       </div>
-      <div class="side divide-layout">
+      <div class="info divide-layout">
         <div>
-          <div class="info"></div>
+          <div class="box">
+            <div class="font-title">TEL</div>
+            <div class="font-body">+886-7-1234567</div>
+          </div>
+          <div class="box mt-10">
+            <div class="font-title">FAX</div>
+            <div class="font-body">+886-7-1234567</div>
+          </div>
+        </div>
+        <div>
+          <div class="box">
+            <div class="font-title">MAIL</div>
+            <div class="font-body">elh@hexschool.com</div>
+          </div>
+          <div class="box mt-10">
+            <div class="font-title">WEB</div>
+            <div class="font-body">www.elhhexschool.com.tw</div>
+          </div>
         </div>
       </div>
     </div>
@@ -20,9 +47,10 @@
 <script lang="ts" setup></script>
 
 <style scoped lang="stylus">
-footer
+
+.footer
   padding 80px 12px
-  background-color #140F0A
+  background-color $bg-color
   color #fff
   .bottom
     margin-top 80px
@@ -30,7 +58,27 @@ footer
     letter-spacing 0.32px
   address
     font-style normal
+  .logo
+    display block
+    width 196px
+  .social-media
+    display flex
+    gap 16px
+    margin-top 40px
+    .link
+      width 40px
+  .info
+    gap 80px
+    +under(sm)
+      margin-top 40px
+      >div + div
+        margin-top 40px
+  .copyright
+    +under(sm)
+      margin-top 16px
 .divide-layout
-  display flex
-  justify-content space-between
+  +over(sm)
+    display flex
+    justify-content space-between
+
 </style>
