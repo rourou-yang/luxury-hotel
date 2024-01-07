6b98e6d11a32b5773e8f69b4910a1130e26233bb
innie <innie902@gmail.com>
2024-01-07 13:45:19 +0800

    style: 調整global container

--- components/PageFooter.vue
+++ components/PageFooter.vue
@@ -1,45 +1,47 @@
 <template>
-  <footer class="web-container footer">
-    <div class="top divide-layout">
-      <div>
-        <nuxt-link to="/" class="logo">
-          <img src="~/assets/images/logo.svg" alt="享樂酒店">
-        </nuxt-link>
-        <div class="social-media">
-          <a href="" class="link">
-            <img src="~/assets/images/icon-line.png" alt="">
-          </a>
-          <a href="" class="link">
-            <img src="~/assets/images/icon-ig.png" alt="">
-          </a>
+  <footer>
+    <div class="web-container">
+      <div class="top divide-layout">
+        <div>
+          <nuxt-link to="/" class="logo">
+            <img src="~/assets/images/logo.svg" alt="享樂酒店">
+          </nuxt-link>
+          <div class="social-media">
+            <a href="" class="link">
+              <img src="~/assets/images/icon-line.png" alt="">
+            </a>
+            <a href="" class="link">
+              <img src="~/assets/images/icon-ig.png" alt="">
+            </a>
+          </div>
+        </div>
+        <div class="info divide-layout">
+          <div>
+            <div class="box">
+              <div class="font-title">TEL</div>
+              <div class="font-body">+886-7-1234567</div>
+            </div>
+            <div class="box mt-10">
+              <div class="font-title">FAX</div>
+              <div class="font-body">+886-7-1234567</div>
+            </div>
+          </div>
+          <div>
+            <div class="box">
+              <div class="font-title">MAIL</div>
+              <div class="font-body">elh@hexschool.com</div>
+            </div>
+            <div class="box mt-10">
+              <div class="font-title">WEB</div>
+              <div class="font-body">www.elhhexschool.com.tw</div>
+            </div>
+          </div>
         </div>
       </div>
-      <div class="info divide-layout">
-        <div>
-          <div class="box">
-            <div class="font-title">TEL</div>
-            <div class="font-body">+886-7-1234567</div>
-          </div>
-          <div class="box mt-10">
-            <div class="font-title">FAX</div>
-            <div class="font-body">+886-7-1234567</div>
-          </div>
-        </div>
-        <div>
-          <div class="box">
-            <div class="font-title">MAIL</div>
-            <div class="font-body">elh@hexschool.com</div>
-          </div>
-          <div class="box mt-10">
-            <div class="font-title">WEB</div>
-            <div class="font-body">www.elhhexschool.com.tw</div>
-          </div>
-        </div>
+      <div class="bottom divide-layout">
+        <address>806023 台灣高雄市新興區六角路123號</address>
+        <div class="copyright">© 享樂酒店 2023 All Rights Reserved.</div>
       </div>
-    </div>
-    <div class="bottom divide-layout">
-      <address>806023 台灣高雄市新興區六角路123號</address>
-      <div class="copyright">© 享樂酒店 2023 All Rights Reserved.</div>
     </div>
   </footer>
 </template>
@@ -48,8 +50,8 @@
 
 <style scoped lang="stylus">
 
-.footer
-  padding 80px 12px
+footer
+  padding 80px 0px
   background-color $bg-color
   color #fff
   .bottom
