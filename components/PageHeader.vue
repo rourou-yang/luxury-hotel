180a4f655b41ac189d13d4ceae5570b4f6a30a15
innie <innie902@gmail.com>
2024-01-09 23:11:01 +0800

    feat: add nav items

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -4,6 +4,17 @@
       <nuxt-link to="/" class="logo">
         <img src="~/assets/images/logo.svg" alt="享樂酒店">
       </nuxt-link>
+      <nav>
+        <v-btn variant="text" color="#BF9D7D" size="large">
+          <nuxt-link  class="nav-link">客房旅宿</nuxt-link>
+        </v-btn>
+        <v-btn variant="text" color="#BF9D7D" size="large">
+          <nuxt-link :to="{name: 'Login'}" class="nav-link">會員登入</nuxt-link>
+        </v-btn>
+        <v-btn color="#BF9D7D" size="large">
+          <nuxt-link  class="nav-link">立即訂房</nuxt-link>
+        </v-btn>
+      </nav>
     </header>
   </div>
 </template>
@@ -26,4 +37,9 @@ header
       height 40px
     img
       height 100%
+nav
+  display flex
+  gap 16px
+  .nav-link
+    color #fff
 </style>
